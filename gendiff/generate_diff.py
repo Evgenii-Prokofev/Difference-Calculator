db71328748fca5cbe590676c594c8a31b30749ae
Евгений <prokofev_619@mail.ru>
2023-11-05 16:22:07 +0300

    add Code Climate bage

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -18,23 +18,23 @@ def generate_diff(file1, file2):
     for key in removed:
         data.update({key: {'prefix': '-'}})
         data[key].update({'name': key})
-        data[key].update({'data':  dict1[key]})
+        data[key].update({'data': dict1[key]})
     for key in added:
         data.update({key: {'prefix': '+'}})
         data[key].update({'name': key})
-        data[key].update({'data':  dict2[key]})
+        data[key].update({'data': dict2[key]})
     for key in kept:
         if dict1[key] == dict2[key]:
-            data.update({key:  {'prefix':  ' '}})
+            data.update({key: {'prefix': ' '}})
             data[key].update({'name': key})
             data[key].update({'data': dict1[key]})
         else:
             data.update({f'{key}1': {'prefix': '-'}})
             data[f'{key}1'].update({'name': key})
-            data[f'{key}1'].update({'data':  dict1[key]})
+            data[f'{key}1'].update({'data': dict1[key]})
             data.update({f'{key}2': {'prefix': '+'}})
             data[f'{key}2'].update({'name': key})
-            data[f'{key}2'].update({'data':  dict2[key]})
+            data[f'{key}2'].update({'data': dict2[key]})
 
     result = dict(sorted(data.items()))
     output = ''
